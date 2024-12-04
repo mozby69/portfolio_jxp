d6a08e94eb484940f5efa6c92717fc94ba504ed2
mozby69 <mozbygreen13@gmail.com>
2024-12-04 10:25:27 +0800

    fourth

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
    distDir: 'out',
+   assetPrefix: '/portfolio_jxp/',
+   basePath: '/portfolio_jxp',
 };
 
 export default nextConfig;
